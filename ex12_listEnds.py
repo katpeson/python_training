180526b08707f96da4eb9bcfcc5806ddaf47d11f
Kati Pesonen <kpesonen@evernote.com>
2018-08-29 15:36:20 +0200

    updated ex12 with second option for solution

--- ex12_listEnds.py
+++ ex12_listEnds.py
@@ -3,7 +3,14 @@
 import random
 
 
-def top_end(list):
+# Option 1
+def top_end1(list):
+    first_last = [list[0], list[len(list) - 1]]
+    return first_last
+
+
+# Option 2, will remove the popped items from original list (was not told to save those)!
+def top_end2(list):
     first_last = []
     first_last.append(list.pop(0))
     first_last.append(list.pop())
@@ -11,6 +18,9 @@ def top_end(list):
 
 
 a = random.sample(range(120), 10)
+# Printing original list
 print (a)
-# printing before first and last removed (was not told to save those!)
-print (top_end(a))
+# Result option 1, running this first since option 2 removes the top and end items from original list
+print (top_end1(a))
+# Result option 2
+print (top_end2(a))
