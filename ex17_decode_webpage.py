06fa3313665dc9846d40af5f941eb37d29800f88
Kati Pesonen <kpesonen@evernote.com>
2018-09-03 12:17:37 +0200

    task 17 ready and tested
    covers only front page of the nytimes.com however

--- ex17_decode_webpage.py
+++ ex17_decode_webpage.py
@@ -1,17 +1,37 @@
 # http://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
 # Task: Use the BeautifulSoup and requests Python packages to print out a list of all the article titles
 # ...on the New York Times homepage
-# TODO: this prints currently NOTHING! Need to recognize articles and collect those and then print their summary as list
+# TODO: could try to expand this to access the other 'top' pages after front one to gather the titles also from there
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 
-url = 'http://www.nytimes.com/'
-r = requests.get(url)
-r_html = r.text
-# now r_html contains the HTML of the page as a string
-soup = BeautifulSoup(r_html, 'html.parser')
-# print (soup.prettify())
-for article in soup.find_all('Article'):
-    print(article.get('summary'))
+# My function to return titles from the main page
+def print_titles(url):
+    nro = 1
+    r = requests.get(url)
+    r_html = r.text
+    # now r_html contains the HTML of the front page as a string
+    soup = bs(r_html, 'html.parser')
+    # lists all articles in the top section of the website (seems the rest of the page is outside html)
+    for tag in soup.find_all('article'):
+        title = tag.h2.string
+        print nro, ":", title
+        nro += 1
+
+
+# # Testing other peoples solution to bring all article titles
+# # returns 'None' on 3.9.2018 so I assume the html has changed and this can no longer locate the article titles
+# def test(url):
+#     res = requests.get(url)
+#     soup = bs(res.text, 'html.parser')
+#     stories = soup.findAll("h2", {"class": "story-heading"})
+#     for i in stories:
+#         cleantitle = i.text.strip()
+#         return(cleantitle)
+
+
+print "Article titles on front page: "
+print_titles('http://www.nytimes.com/')
+# print test('http://www.nytimes.com/')
